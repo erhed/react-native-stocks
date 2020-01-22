9ecc3424284ee66e14002c67ada045e5fad2f65f
erhed <erik.hedrenius@gmail.com>
2020-01-23 00:25:48 +0100

    Add FlatList, TextInput & Yahoo logo to HomeScreen

---
+++ src/components/ListItem/ListItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ListItem = ({text}) => {
+  return(
+    <View style={styles.container}>
+      <Text style={styles.text}>{text}</Text>
+    </View>
+  );
+}
+
+export default ListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingBottom: 10,
+    paddingTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#CCCCCC',
+  },
+  text: {
+    fontSize: 16,
+  }
+});
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,64 @@
 import React, { Component } from 'react';
-import { SafeAreaView, Text, TextInput, StyleSheet, View } from 'react-native';
+import { SafeAreaView, Text, TextInput, StyleSheet, View, Image, Keyboard, FlatList } from 'react-native';
+import ListItem from '../components/ListItem/ListItem';
 
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
+
+    this.listData = [
+      {
+        title: 'nummer ett',
+        id: '1'
+      },
+      {
+        title: 'nummer två',
+        id: '2'
+      },
+      {
+        title: 'nummer tre',
+        id: '3'
+      },
+    ];
+
+    this.state = {
+      text: 'hej',
+      listData: this.listData
+    }
+  }
+
+  onInputTextChange = text => {
+    if (text === '') {
+      Keyboard.dismiss();
+    }
   }
 
   render() {
-    return(
+    return (
       <SafeAreaView style={styles.screen}>
-      <View style={styles.searchContainer}>
-        <TextInput 
-          style={styles.searchTextInput}
-          placeholder="Search asset" />
-      </View>
+        <View style={styles.logoContainer}>
+          <Image
+            style={{ width: 205, height: 75 }}
+            source={require('../assets/yahoo-finance-logo.jpg')}
+          />
+        </View>
+        <View style={styles.searchContainer}>
+          <TextInput
+            style={styles.searchTextInput}
+            placeholder='Search asset'
+            placeholderTextColor='#AAAAAA'
+            clearButtonMode='while-editing'
+            onChangeText={text => this.onInputTextChange(text)} />
+        </View>
+        {/* <Text>{this.state.text}</Text> */}
+        <View style={styles.listContainer}>
+          <FlatList
+            style={styles.list}
+            data={this.state.listData}
+            renderItem={({ item }) => <ListItem text={item.title} />}
+            keyExtractor={item => item.id}
+          />
+        </View>
       </SafeAreaView>
     );
   }
@@ -22,7 +67,13 @@ export default class HomeScreen extends Component {
 const styles = StyleSheet.create({
   screen: {
     backgroundColor: '#FFFFFF',
-    flex: 1
+    flex: 1,
+  },
+  logoContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 50,
+    marginBottom: 40,
   },
   searchContainer: {
     margin: 20,
@@ -31,7 +82,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
     padding: 12,
     borderBottomWidth: 2,
-    borderBottomColor: '#FF0000',
+    borderBottomColor: '#0000FF',
     fontSize: 16,
+  },
+  listContainer: {
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 50,
   }
 });
