feeba884b84c67f98d2779086d19d2aeeb91dfb0
erhed <erik.hedrenius@gmail.com>
2020-01-22 23:34:33 +0100

    Add HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { SafeAreaView, Text, TextInput, StyleSheet, View } from 'react-native';
 
 export default class HomeScreen extends Component {
   constructor(props) {
@@ -8,9 +8,30 @@ export default class HomeScreen extends Component {
 
   render() {
     return(
-      <SafeAreaView>
-        <Text>HomeScreen</Text>
+      <SafeAreaView style={styles.screen}>
+      <View style={styles.searchContainer}>
+        <TextInput 
+          style={styles.searchTextInput}
+          placeholder="Search asset" />
+      </View>
       </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    backgroundColor: '#FFFFFF',
+    flex: 1
+  },
+  searchContainer: {
+    margin: 20,
+  },
+  searchTextInput: {
+    backgroundColor: '#FFFFFF',
+    padding: 12,
+    borderBottomWidth: 2,
+    borderBottomColor: '#FF0000',
+    fontSize: 16,
+  }
+});
