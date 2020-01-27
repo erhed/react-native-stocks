32a2d5b0eaae546091e42fc60c7fdcb1546196c8
erhed <erik.hedrenius@gmail.com>
2020-01-27 14:27:46 +0100

    Get prices API

---
+++ src/api/Price.js
@@ -0,0 +1,40 @@
+import { AlphaVantageKey as apiKey } from './Keys';
+
+export async function getPrices(symbol) {
+  return new Promise(function (resolve, reject) {
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${apiKey}`;
+    fetch(url)
+      .then(resp => {
+        return resp.json();
+      }).then(data => {
+        let allDailyPrices = Object.values(data)[1];
+        let latestDailyOCHLPrices = Object.values(allDailyPrices)[0];
+        let latestDailyClosePrice = Object.values(latestDailyOCHLPrices)[3];
+
+        let chartDates = [];
+        let chartDailyClosePrices = [];
+
+        for (let i = 0; i < 20; i++) {
+          let dailyOCHLPrices = Object.values(allDailyPrices)[i];
+          if (dailyOCHLPrices != undefined) {
+            let date = Object.keys(allDailyPrices)[i];
+            chartDates.push(date);
+            let dailyClosePrice = Object.values(dailyOCHLPrices)[3];
+            chartDailyClosePrices.push(parseFloat(dailyClosePrice));
+          } else {
+            break;
+          }
+        }
+
+        let responseObject = {
+          dates: chartDates,
+          prices: chartDailyClosePrices,
+        }
+
+        resolve(responseObject);
+      })
+      .catch(error => {
+        reject({ status: 'error', error: error });
+      });
+  });
+}
--- src/screens/ChartScreen.js
+++ src/screens/ChartScreen.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { SafeAreaView, Text, StyleSheet, StatusBar } from 'react-native';
+import { SafeAreaView, Text, StyleSheet, StatusBar, View } from 'react-native';
+import { getPrices } from '../api/Price';
 
 export default class ChartScreen extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
       symbol: '',
       name: '',
@@ -18,14 +19,21 @@ export default class ChartScreen extends Component {
       symbol: symbol,
       name: name,
     });
+
+    let data = await getPrices(symbol);
+    console.warn(data);
   }
 
   render() {
     return (
       <SafeAreaView style={styles.screen}>
         <StatusBar barStyle="dark-content" />
-        <Text>{this.state.symbol}</Text>
-        <Text>{this.state.name}</Text>
+        <View style={styles.container}>
+          <Text style={styles.headerName}>{this.state.name}</Text>
+          <Text style={styles.headerSymbol}>{this.state.symbol}</Text>
+          
+        </View>
+
       </SafeAreaView>
     );
   }
@@ -35,5 +43,23 @@ const styles = StyleSheet.create({
   screen: {
     backgroundColor: '#FFF',
     flex: 1
-  }
+  },
+  container: {
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 10,
+  },
+  headerName: {
+    fontSize: 30,
+    fontWeight: '800',
+    marginBottom: 0,
+    color: 'rgb(55, 1, 125)',
+  },
+  headerSymbol: {
+    fontSize: 22,
+    fontWeight: '300',
+    marginBottom: 10,
+    color: 'rgb(55, 1, 125)',
+  },
+
 });
